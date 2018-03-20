8bd3f88d06d478e3913f5dfc229f695de81cf6b7
Mario Mucalo <mucalo.mario@gmail.com>
2018-03-20 23:10:31 +0100

    Readding Backend

---
+++ Backend/Backend/Controllers/WorkoutsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Workouts")]
+    [Authorize]
+    public class WorkoutsController : Controller
+    {
+        private readonly WorkoutContext _context;
+
+        public WorkoutsController(WorkoutContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Workouts
+        [HttpGet]
+        public IEnumerable<Workout> GetWorkout()
+        {
+            return _context.Workout.OrderByDescending(d => d.Date);
+        }
+
+        // GET: api/Workouts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWorkout([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var workout = await _context.Workout.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(workout);
+        }
+
+        // PUT: api/Workouts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorkout([FromRoute] int id, [FromBody] Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != workout.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(workout).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkoutExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Workouts
+        [HttpPost]
+        public async Task<IActionResult> PostWorkout([FromBody] Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Workout.Add(workout);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWorkout", new { id = workout.Id }, workout);
+        }
+
+        // DELETE: api/Workouts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWorkout([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var workout = await _context.Workout.SingleOrDefaultAsync(m => m.Id == id);
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            _context.Workout.Remove(workout);
+            await _context.SaveChangesAsync();
+
+            return Ok(workout);
+        }
+
+        private bool WorkoutExists(int id)
+        {
+            return _context.Workout.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Backend/Backend/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public int DistanceInMeters { get; set; }
+        [Required]
+        public long TimeInSeconds { get; set; }
+    }
+}
